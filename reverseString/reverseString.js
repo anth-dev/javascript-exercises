987a409c5159d736a91fdfc6cac6e2e334ccb9ea
Anthony Giles <omgliekpie@gmail.com>
2019-11-19 13:44:19 -0700

    declare variables for function, start building loop

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let characterArray = inputString.split('');
+    let iterations = characterArray.length;
+    while (iterations < 0) {
+        
+    }
+    return characterArray + iterations;
 }
 
 module.exports = reverseString
