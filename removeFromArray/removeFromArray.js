9de208cf8c72e3f7b9c11d2aa8812149660bbc76
Anthony Giles <omgliekpie@gmail.com>
2019-11-21 16:33:28 -0700

    make itemsToRemove arguments into an array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+// removeFromArray ([1,2,3,4], ...itemsToRemove) should return an
+// array with the itemsToRemove removed
 
+const removeFromArray = function(numberArray, ...itemsToRemove) {
+    let toRemove = Array.from(itemsToRemove);
+    return toRemove;
 }
 
 module.exports = removeFromArray
