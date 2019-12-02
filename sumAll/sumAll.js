f371226ccc588ea961a1d9eb2958cfb7d1dde6a0
Anthony Giles <omgliekpie@gmail.com>
2019-12-02 14:47:11 -0700

    declare variables and write a for loop

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    mySum = 0;
+    for (i = secondNumber; i < firstNumber; i--) {
+        console.log('i = ' + i);
+        mySum += i;
+    }
+    return mySum;
 }
 
 module.exports = sumAll
