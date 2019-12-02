34e8c8caa5089877d2d786390d8d61363a8703ae
Anthony Giles <omgliekpie@gmail.com>
2019-12-02 14:53:31 -0700

    change loop exit condition from < to >=

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(firstNumber, secondNumber) {
     mySum = 0;
-    for (i = secondNumber; i < firstNumber; i--) {
+    for (i = secondNumber; i > firstNumber; i--) {
         console.log('i = ' + i);
         mySum += i;
     }
