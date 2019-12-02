fb417f6b9883537c265d8979ab4d89d066ec88d5
Anthony Giles <omgliekpie@gmail.com>
2019-12-02 16:25:01 -0700

    remove unneeded comments

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,13 +1,10 @@
 const sumAll = function(firstNumber, secondNumber) {
     mySum = 0;
-    // check to see if both args are numbers, return 'ERROR' if not
     if (typeof(firstNumber) != 'number' || typeof(secondNumber) != 'number') {
         return 'ERROR';
-    // check to see if both args are positive, return 'ERROR' if not
     } else if (firstNumber < 0 || secondNumber < 0) {
         return 'ERROR';
     } else if (firstNumber > secondNumber) {
-        // if firstNumber is bigger, swap the args
         for (i = firstNumber; i >= secondNumber; i--) {
             mySum += i;
         }
