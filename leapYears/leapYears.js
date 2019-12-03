41ee6c9190bc14ab260ee8ea58d0791c11837c84
Anthony Giles <omgliekpie@gmail.com>
2019-12-03 13:24:08 -0700

    add leap year information for reference

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+// Leap years are divisible by four. However, years
+// divisible by 100 are not leap years unless they are
+// divisible by 400.
 
+const leapYears = function() {
+    
 }
 
 module.exports = leapYears
