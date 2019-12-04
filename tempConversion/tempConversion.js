da16075eb6017dc47f077629c77cfc63b8342793
Anthony Giles <omgliekpie@gmail.com>
2019-12-03 18:20:36 -0700

    code other function, passes all tests

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
 // C = (F - 32) * 5/9
-const ftoc = function() {
-
+const ftoc = function(degreesFarenheit) {
+  let c = (degreesFarenheit - 32) * 5/9;
+  return Math.round(c * 10) / 10;
 }
 
 // F = C * 9/5 + 32
-const ctof = function() {
-
+const ctof = function(degreesCelsius) {
+  let f = degreesCelsius * 9/5 + 32;
+  return Math.round(f * 10) / 10;
 }
 
 module.exports = {
