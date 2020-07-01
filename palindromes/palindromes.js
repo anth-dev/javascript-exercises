ca95bf4dec72815f258b8973811973a3352b4903
Anthony Giles <omgliekpie@gmail.com>
2020-07-01 08:49:41 -0600

    add comments about approach to problem

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
+const palindromes = function(string) {
+    // split the string into an array
 
+    // reverse the array order and output as a string
+
+    // compare generated string with original string
+    // and output true or false
 }
 
 module.exports = palindromes
