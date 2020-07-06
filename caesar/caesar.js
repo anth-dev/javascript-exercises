db86cd73ed11423afd028ced76230d2e18441ad1
Anthony Giles <omgliekpie@gmail.com>
2020-07-06 10:24:35 -0600

    make alphabet array and spread string over new array

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,4 +1,18 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+    // array containing alphabet
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    // blank array to build new string
+    let encryptedString = [];
+
+    // break string into characters
+    let stringArray = [...string];
+    
+    // find character in array and determine lower or upper case
+
+    // use alphabet array to find shifted characters and reapply case
+
+    // return encrypted string
 
 }
 
