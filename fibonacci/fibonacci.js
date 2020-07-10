d976a19f78f51723be9e2dad7c27c85cf8371362
Anthony Giles <omgliekpie@gmail.com>
2020-07-10 11:46:54 -0600

    code the addition algorithm

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let firstAddend = 1;
+    let secondAddend = 1;
+    let sum;
+    for (i = 2; i < number; i++) {
+        sum = firstAddend + secondAddend;
+        firstAddend = secondAddend;
+        secondAddend = sum;
+    }
+    return sum;
 }
 
 module.exports = fibonacci
