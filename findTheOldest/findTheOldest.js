657cfba15ed630e3165c846548a12fa82960d301
Anthony Giles <omgliekpie@gmail.com>
2020-07-13 18:50:48 -0600

    pass first test

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+    people.forEach(person => person.age = person.yearOfDeath - person.yearOfBirth);
+    people.sort(function(a, b){return a.age - b.age});
+    return people[people.length-1];
 }
 
 module.exports = findTheOldest
