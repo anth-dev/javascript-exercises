6620530851a081a1ef837f84f7727c0ebec559ed
Anthony Giles <omgliekpie@gmail.com>
2020-07-16 11:35:05 -0600

    get and store current year

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,4 +1,6 @@
 let findTheOldest = function(people) {
+    let thisYear = new Date().getFullYear();
+    console.log(thisYear);
     people.forEach(person => person.age = person.yearOfDeath - person.yearOfBirth);
     people.sort(function(a, b){return a.age - b.age});
     return people[people.length-1];
