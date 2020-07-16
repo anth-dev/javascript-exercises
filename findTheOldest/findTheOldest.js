927524a2a4711567df6f63b6c32c62eeff101ec4
Anthony Giles <omgliekpie@gmail.com>
2020-07-16 12:44:16 -0600

    complete exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,8 +1,15 @@
 let findTheOldest = function(people) {
     let thisYear = new Date().getFullYear();
     console.log(thisYear);
-    people.forEach(person => person.age = person.yearOfDeath - person.yearOfBirth);
+    people.forEach(person => {
+        if (!person.yearOfDeath) {
+            person.age = thisYear - person.yearOfBirth;
+        } else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+    });
     people.sort(function(a, b){return a.age - b.age});
+    console.log(people);
     return people[people.length-1];
 }
 
